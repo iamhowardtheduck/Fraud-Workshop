dd2cde0651aa2c8902b32d0a9a6723cf1f421e9e
Peter Titov <11168913+iamhowardtheduck@users.noreply.github.com>
2025-11-23 08:57:43 -0500

    Update and rename install_aml_generator_root.sh to fraud-gen.sh

--- Scripts/fraud-gen.sh
+++ Scripts/fraud-gen.sh
@@ -1,4 +1,4 @@
 pip install pandas numpy elasticsearch
 python3 /root/Fraud-Workshop/Scripts/wire-fraud.py
 python3 /root/Fraud-Workshop/Scripts/money-laundering.py
-#python3 /root/Fraud-Workshop/Scripts/fraud_workshop.py
+
