7f01aaebd579079afd66e3411abbea2756e188e1
Peter Titov <11168913+iamhowardtheduck@users.noreply.github.com>
2026-08-07 10:41:16 -0400

    Update fraud-gen.sh

--- Scripts/V2/fraud-gen.sh
+++ Scripts/V2/fraud-gen.sh
@@ -5,3 +5,4 @@ pip install pandas numpy elasticsearch faker
 python3 /home/elastic/Fraud-Workshop/Scripts/V2/wire-fraud.py
 python3 /home/elastic/Fraud-Workshop/Scripts/V2/money-laundering.py
 python3 /home/elastic/Fraud-Workshop/Scripts/V2/smurfing.py
+python3 /home/elastic/Fraud-Workshop/Scripts/V2/brokerage_workshop.py
